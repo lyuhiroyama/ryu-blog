86eadd1a03a2282122a40f5bae0283478950a08d
lyuhiroyama <lyuhiroyama@gmail.com>
2025-04-02 00:40:07 +0900

    gql: define root schema

---
+++ backend/app/graphql/graphql_schema.rb
@@ -0,0 +1,11 @@
+require 'graphql'
+require_relative 'types/package_type'
+require_relative 'types/user_type'
+require_relative 'types/query_type'
+require_relative 'types/mutation_type'
+
+# root structure of GraphQL API
+class ShipmentTrackerSchema < GraphQL::Schema
+  query Types::QueryType
+  query Types::MutationType
+end
