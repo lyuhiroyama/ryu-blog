434ae4d940cf66e1d327891d2eeecab20dbcf93a
lyuhiroyama <lyuhiroyama@gmail.com>
2025-04-03 15:16:57 +0900

    refactor: Delete PackageTracker component
    - Change PackageDetails to show in separate container beneath data input field container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import './App.scss';
-import PackageTracker from './components/PackageTracker';
+import { useState } from 'react';
+// import PackageTracker from './components/PackageTracker';
+import PackageSearchForm from './components/PackageSearchForm';
+import PackageCreateForm from './components/PackageCreateForm';
+import PackageDetails from './components/PackageDetails';
 
 function App() {
+  const [packageData, setPackageData] = useState(null);
+
   return (
-    <div className='app-div'>
-      <PackageTracker />
+    <div className='app-component'>
+      <div className='header-container'>
+        <h1 className='header-title'>Shipment Tracker</h1>
+      </div>
+      <div className='input-container'>
+        <PackageSearchForm setPackageData={setPackageData} />
+        <PackageCreateForm />
+      </div>
+      <div className='data-display-container'>
+        {packageData && <PackageDetails packageData={packageData} />}
+      </div>
     </div>
   );
 }
--- src/components/PackageSearchForm.tsx
+++ src/components/PackageSearchForm.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { gql, useLazyQuery } from '@apollo/client';
-import PackageDetails from './PackageDetails';
+import React, { useState } from "react";
+import { gql, useLazyQuery } from "@apollo/client";
 
 // Define GraphQL query (Within it, you specify the shape of the data you want to retrieve.)
 const GET_PACKAGE = gql`
@@ -18,41 +17,40 @@ const GET_PACKAGE = gql`
     }
 `;
 
-
-
-const PackageSearchForm: React.FC = () => {
-    const [trackingNumber, setTrackingNumber] = useState('');
-    const [getPackage, { loading, error, data}] = useLazyQuery(GET_PACKAGE);
+const PackageSearchForm: React.FC<{ setPackageData: (data: any) => void }> = ({
+    setPackageData
+}) => {
+    const [trackingNumber, setTrackingNumber] = useState("");
+    const [getPackage, { loading, error, data }] = useLazyQuery(GET_PACKAGE);
     // useLazyQuery() returns a tuple with two elements:
     // Function to execute query with & Object containing the query's state.
     // loading -> boolean, error -> obj, data -> obj (contains data returned by GraphQL query)
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        getPackage({ variables: {trackingNumber: trackingNumber}});  // Pass 'options object' as arg.
-    }
-    
+        getPackage({ variables: { trackingNumber: trackingNumber } }); // Pass 'options object' as arg.
+        if (data && data.package) {
+            setPackageData(data.package);
+        }
+    };
+
     return (
         <div className="package-search">
             <h2>Track your package</h2>
 
             <form onSubmit={handleSubmit}>
-                <input 
+                <input
                     type="text"
                     value={trackingNumber}
-                    placeholder='Enter tracking number'
+                    placeholder="Enter tracking number"
                     onChange={(e) => setTrackingNumber(e.target.value)}
                     required
                 />
                 <button type="submit">Track Package</button>
             </form>
-            
 
             {loading && <p>Loading...</p>}
             {error && <p>Error: {error.message}</p>}
-            {data && data.package && <PackageDetails packageData={data.package} />}
-            {/* note: 'package' here is the query field you defined in your GraphQL schema: */}
-
         </div>
     );
 };
